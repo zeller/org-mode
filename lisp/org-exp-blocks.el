8a0988fa5bc2491930818047acc717641037d9b6
Michael Zeller <me@michaelzeller.com>
2009-10-20 16:20:03 -0700

    Escaped the output-directory for use within setwd call

--- lisp/org-exp-blocks.el
+++ lisp/org-exp-blocks.el
@@ -371,7 +371,7 @@ other backends, it converts the comment into an EXAMPLE segment."
   (interblock-initiate-R-buffer)
   (let ((out-directory (format "%sout" (file-name-directory filename))))
     (if (not (file-exists-p out-directory)) (mkdir out-directory))
-    (interblock-R-input-command (format "setwd('%s')" out-directory))))
+    (interblock-R-input-command (format "setwd('%S')" out-directory))))
 
 (defun org-export-blocks-format-R (body &rest headers)
   "Process R blocks and replace \R{} forms outside the blocks
